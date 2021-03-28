61b62253438b2f8bfd006fa3b3d18183ddf1965c
sandraUni <74244274+sandraUni@users.noreply.github.com>
2021-03-28 19:39:56 +0200

    added dates in zadanie3

--- Lista 4/src/lista4/zadanie3.java
+++ Lista 4/src/lista4/zadanie3.java
@@ -1,10 +1,28 @@
 package lista4;
 
+import java.util.Scanner;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class zadanie3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc=new Scanner(System.in);
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+		
+		System.out.print("Enter the first date in the format dd.MM.yyyy: ");
+		//String date = sc.nextLine();
+		
+		
+		
+		Date date1=simpleDateFormat.parse(sc.nextLine());
+		
+		System.out.print(simpleDateFormat.format(date1));
+		sc.close();
 	}
 
 }
