9ce49c123e4076a766467d147f3b3b165289d2db
sandraUni <74244274+sandraUni@users.noreply.github.com>
2021-03-28 21:56:57 +0200

    added ready zadanie3

--- Lista 4/src/lista4/zadanie3.java
+++ Lista 4/src/lista4/zadanie3.java
@@ -13,15 +13,41 @@ public class zadanie3 {
 		Scanner sc=new Scanner(System.in);
 		
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+		Date date1, date2;
+		Date firstDate, secondDate;
+		long numberOfDays=0; //iloúÊ dni
+		try
+		{
+			System.out.print("Enter the first date in the format dd.MM.yyyy: ");
+			date1=simpleDateFormat.parse(sc.nextLine());
+			
+			System.out.print("Enter the second date in the format dd.MM.yyyy: ");
+			date2=simpleDateFormat.parse(sc.nextLine());
+			
+			if (date1.before(date2) )
+			{
+				firstDate=date1;
+				secondDate=date2;
+			}
+			else
+			{
+				firstDate=date2;
+				secondDate=date1;
+			}
+			
+			numberOfDays = (secondDate.getTime() - firstDate.getTime())/(24*1000*60*60);
+			System.out.println("Number of days: " + numberOfDays);
+			
+			System.out.println("First date: " + simpleDateFormat.format(firstDate));
+			System.out.println("Second day: " + simpleDateFormat.format(secondDate));
+		}
+		catch (ParseException e) {
+			System.out.println("èle");
+		}
 		
-		System.out.print("Enter the first date in the format dd.MM.yyyy: ");
-		//String date = sc.nextLine();
 		
 		
 		
-		Date date1=simpleDateFormat.parse(sc.nextLine());
-		
-		System.out.print(simpleDateFormat.format(date1));
 		sc.close();
 	}
 
