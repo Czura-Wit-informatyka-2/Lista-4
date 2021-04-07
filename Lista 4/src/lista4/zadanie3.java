aea48b3195af99e22a931404dfbb326a17036374
sandraUni <74244274+sandraUni@users.noreply.github.com>
2021-04-08 01:37:50 +0200

    added changes in zadanie3

--- Lista 4/src/lista4/zadanie3.java
+++ Lista 4/src/lista4/zadanie3.java
@@ -7,46 +7,34 @@ import java.text.SimpleDateFormat;
 
 public class zadanie3 {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException {  
 		// TODO Auto-generated method stub
 		
 		Scanner sc=new Scanner(System.in);
 		
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-		Date date1, date2;
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy"); 
 		Date firstDate, secondDate;
 		long numberOfDays=0; //iloúÊ dni
-		try
+		
+		System.out.print("Enter the first date in the format dd.MM.yyyy: ");
+		Date date1=simpleDateFormat.parse(sc.nextLine());  
+		
+		System.out.print("Enter the second date in the format dd.MM.yyyy: ");
+		Date date2=simpleDateFormat.parse(sc.nextLine());
+		
+		if (date1.before(date2) ) 
 		{
-			System.out.print("Enter the first date in the format dd.MM.yyyy: ");
-			date1=simpleDateFormat.parse(sc.nextLine());
-			
-			System.out.print("Enter the second date in the format dd.MM.yyyy: ");
-			date2=simpleDateFormat.parse(sc.nextLine());
-			
-			if (date1.before(date2) )
-			{
-				firstDate=date1;
-				secondDate=date2;
-			}
-			else
-			{
-				firstDate=date2;
-				secondDate=date1;
-			}
-			
-			numberOfDays = (secondDate.getTime() - firstDate.getTime())/(24*1000*60*60);
-			System.out.println("Number of days: " + numberOfDays);
-			
-			System.out.println("First date: " + simpleDateFormat.format(firstDate));
-			System.out.println("Second day: " + simpleDateFormat.format(secondDate));
+			firstDate=date1;
+			secondDate=date2;
 		}
-		catch (ParseException e) {
-			System.out.println("èle");
+		else
+		{
+			firstDate=date2;
+			secondDate=date1;
 		}
 		
-		
-		
+		numberOfDays = (secondDate.getTime() - firstDate.getTime())/(24*1000*60*60); 
+		System.out.println("Number of days: " + numberOfDays);
 		
 		sc.close();
 	}
